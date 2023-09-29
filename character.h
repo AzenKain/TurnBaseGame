891eee8e55b8de06cb9046d6bcc03cab065df104
KnTheBoi <kntheboi@gmail.com>
2023-09-29 13:24:18 +0700

    minor update 2

--- character.h
+++ character.h
@@ -7,13 +7,23 @@ class character{
         float hp;
         float exp;
         int level;
+        float damage
     public:
-        character(int id, string name, float hp, float exp, int level){
-            this->id = 0;
+        character(){
+            this->id = -1;
             this->name = "UNKNOWN";
             this->hp = 0;
+            this->damage = 0;
             this->exp = 0;
             this->level = 0;
+        }
+        character(int id, string name, float hp, float damage, float exp, int level){
+            this->id = id;
+            this->name = name;
+            this->hp = hp;
+            this->damage = damage;
+            this->exp = exp;   
+            this->level = level;
         }
 
 };
