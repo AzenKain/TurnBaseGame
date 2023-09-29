3be183f15c4ec1358be5fc70ad453dd000cbe64c
KnTheBoi <kntheboi@gmail.com>
2023-09-29 12:49:10 +0700

    minor update

--- character.h
+++ character.h
@@ -1,13 +1,20 @@
 #ifdef CHARACTER_H
 #define CHARACTER.H
 class character{
-private:
-int id;
-string name;
-float hp;
-int exp;
-int level;
-public:
+    private:
+        int id;
+        string name;
+        float hp;
+        float exp;
+        int level;
+    public:
+        character(int id, string name, float hp, float exp, int level){
+            this->id = 0;
+            this->name = "UNKNOWN";
+            this->hp = 0;
+            this->exp = 0;
+            this->level = 0;
+        }
 
 };
 #endif;
